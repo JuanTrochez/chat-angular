e2329d6069e9b96603c41d6803d90e2e149ad6ce
Trochez <juan.trochez95@gmail.com>
2017-03-24 11:19:30 +0100

    [init] structure

---
+++ app/controllers/mainCtrl.js
@@ -0,0 +1,19 @@
+
+app.controller('MainController', ['$scope', '$anchorScroll', '$location', function ($scope, $anchorScroll, $location) {
+        $scope.userMessage = '';
+        $scope.messages = [];
+        var socket = io();
+
+        $scope.sendMessage = function () {
+            socket.emit('chat_message', $scope.userMessage);
+            $scope.userMessage = '';
+        };
+
+        socket.on('chat_message', function (data) {
+            console.log('Message received:', data);
+            $scope.messages.push(data);
+            $scope.$apply();
+            $location.hash('tampon');
+            $anchorScroll();
+        });
+    }]);
---
+++ app/routes/routes.js
@@ -0,0 +1,19 @@
+app.config(function ($stateProvider) {
+//    var templateFolder = "app/templates/client/";    
+//    
+//    var clientState = {
+//        name: 'clients',
+//        url: '/clients',
+//        templateUrl: templateFolder + "clients.html"
+//    }
+//    
+//    var clientShowState = {
+//        name: 'clientsShow',
+//        url: '/clients/{id:int}',
+//        templateUrl: templateFolder + "clients-show.html",
+//        controller:
+//    }
+//    
+//    $stateProvider.state(clientState);
+//    $stateProvider.state(clientShowState);
+});
