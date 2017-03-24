ad9433c6ef74adaf2a92286fde1976b8708861e3
Trochez <juan.trochez95@gmail.com>
2017-03-24 17:17:50 +0100

    [bdd] rows

--- data/chat_angular.sql
+++ data/chat_angular.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1
--- http://www.phpmyadmin.net
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
 --
--- Client :  localhost
--- Généré le :  Ven 24 Mars 2017 à 10:27
+-- Client :  localhost:3306
+-- Généré le :  Ven 24 Mars 2017 à 17:17
 -- Version du serveur :  5.7.11
--- Version de PHP :  5.6.18
+-- Version de PHP :  7.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -31,6 +31,15 @@ CREATE TABLE `channel` (
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Contenu de la table `channel`
+--
+
+INSERT INTO `channel` (`id`, `name`) VALUES
+(1, 'private'),
+(2, 'general'),
+(3, 'BlaBla');
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +55,18 @@ CREATE TABLE `message` (
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Contenu de la table `message`
+--
+
+INSERT INTO `message` (`id`, `message`, `from_id`, `to_id`, `channel_id`, `date`) VALUES
+(101, 'hello', 2, 1, 2, '2017-03-24 16:08:16'),
+(102, 'I post some messages', 2, 1, 2, '2017-03-24 16:09:05'),
+(103, 'This is blabla', 2, 1, 3, '2017-03-24 16:09:18'),
+(104, 'blabla is good', 3, 1, 3, '2017-03-24 16:09:47'),
+(105, 'i post some messages too', 8, 1, 2, '2017-03-24 16:08:51'),
+(106, 'this is a test', 8, 1, 2, '2017-03-24 16:10:06');
+
 -- --------------------------------------------------------
 
 --
@@ -57,6 +78,16 @@ CREATE TABLE `user` (
   `pseudo` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `pseudo`, `password`) VALUES
+(1, 'no_user', 'no_user'),
+(2, 'lolilol', 'lolilol'),
+(3, 'hello', 'world'),
+(8, 'player2', '123456');
 
 --
 -- Index pour les tables exportées
@@ -91,17 +122,17 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `channel`
 --
 ALTER TABLE `channel`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT pour la table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- Contraintes pour les tables exportées
 --
